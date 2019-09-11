4cbfc5174e66aa34f53030bf93f32faeadd7f7d5
Алина Ткаченко <aline8crawford@gmail.com>
2019-09-11 22:39:26 +0300

    Strategy pattern

---
+++ Strategy/Strategy/Duck.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public abstract class Duck
+    {
+        public FlyBehavior flyBehavior;
+        public QuackBehavior quackBehavior;
+
+        public Duck()
+        {
+        }
+
+        public abstract void display();
+
+        public void performFly()
+        {
+            flyBehavior.fly();
+        }
+
+        public void performQuack()
+        {
+            quackBehavior.quack();
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+
+        public void setFlyBehavior(FlyBehavior flyBehavior)
+        {
+            this.flyBehavior = flyBehavior;
+        }
+
+        public void setQuackBehavior(QuackBehavior quackBehavior)
+        {
+            this.quackBehavior = quackBehavior;
+        }
+    }
+}
---
+++ Strategy/Strategy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.performFly();
+            mallard.performQuack();
+
+            ModelDuck modelDuck = new ModelDuck();
+            modelDuck.performFly();
+            modelDuck.setFlyBehavior(new FlyRocketPowered());
+            modelDuck.performFly();
+            Console.ReadKey();
+        }
+    }
+}
